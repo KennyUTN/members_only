fb214f4189223a70477f070eaf971fe0a05e90c5
KennyUTN <asisignacio@gmail.com>
2021-04-12 17:05:10 -0300

    linters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,27 +7,19 @@ module ApplicationHelper
     render partial: 'flash_notice' if flash[:notice]
   end
 
-  def new_post_helper
-    link_to 'New Post', root_path, class: 'text-white nav-item nav-link' if user_signed_in?
-  end
-
   def edit_user_helper
-    if user_signed_in?
-      link_to current_user.name, edit_user_registration_path, class: 'control button is-info'
-    end
+    link_to current_user.name, edit_user_registration_path, class: 'control button is-info' if user_signed_in?
   end
 
   def logout_helper
-    if user_signed_in?
-      link_to 'Logout', destroy_user_session_path, method: :delete, class: 'control button is-info'
-    end
+    link_to 'Logout', destroy_user_session_path, method: :delete, class: 'control button is-info' if user_signed_in?
   end
 
   def sign_in
-      link_to 'Sign In', new_user_session_path, class: 'control button is-info' unless user_signed_in?
+    link_to 'Sign In', new_user_session_path, class: 'control button is-info' unless user_signed_in?
   end
 
   def sign_up
-      link_to 'Sign Up', new_user_registration_path, class: 'control button is-info' unless user_signed_in?
+    link_to 'Sign Up', new_user_registration_path, class: 'control button is-info' unless user_signed_in?
   end
 end
--- app/helpers/feed_helper.rb
+++ app/helpers/feed_helper.rb
@@ -2,18 +2,20 @@ module FeedHelper
   def profile_helper
     render partial: 'profiles' if user_signed_in?
   end
+
   def noprofile_helper
-    render partial: 'notlogged' if not user_signed_in?
+    render partial: 'notlogged' unless user_signed_in?
   end
+
   def new_post_helper
     render partial: 'newpost' if user_signed_in?
   end
 
   def name_and_username_helper(post)
-    render partial: "name_and_username", locals: {:post => post} if user_signed_in?
+    render partial: 'name_and_username', locals: { post: post } if user_signed_in?
   end
 
-  def own_post_buttons_helper (post)
-    render partial: 'own_post_buttons', locals: {:post => post}  if user_signed_in? && current_user.id == post.user_id
+  def own_post_buttons_helper(post)
+    render partial: 'own_post_buttons', locals: { post: post }  if user_signed_in? && current_user.id == post.user_id
   end
 end
